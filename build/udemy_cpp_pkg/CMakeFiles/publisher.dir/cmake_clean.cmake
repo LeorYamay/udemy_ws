d8dde36e09db0697614ba302623a21a35b1bcf8d
= <=>
2023-11-07 10:50:48 -0800

    18. compiling ROS Nodes

---
+++ build/udemy_cpp_pkg/CMakeFiles/publisher.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "/home/leory/udemy_ws/devel/lib/udemy_cpp_pkg/publisher"
+  "/home/leory/udemy_ws/devel/lib/udemy_cpp_pkg/publisher.pdb"
+  "CMakeFiles/publisher.dir/src/publisher.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/publisher.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
